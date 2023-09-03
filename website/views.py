915d818347275b9716923e8d645343f00d98f747
BigBadSheep <bigbadsheeep@gmail.com>
2023-09-03 04:19:30 +0200

    nowy brancz
    dodanie nowego brancza

--- website/views.py
+++ website/views.py
@@ -24,6 +24,7 @@ def home():
     return render_template("home.html", user=current_user)
 
 
+
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
