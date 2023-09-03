b80f7ec465a5af0d95f470ba31548f0fd701d162
BigBadSheep <bigbadsheeep@gmail.com>
2023-09-03 03:18:52 +0200

    błędy
    dlaczego to niedziała?

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'kochambardzolinuxa'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -10,5 +19,13 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    cerate_database(app)
 
     return app
+
+def cerate_database(app):
+    if not path('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
