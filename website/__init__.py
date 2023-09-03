c0e2473496bc2e740c600d7493f4b04b5e3566c5
BigBadSheep <bigbadsheeep@gmail.com>
2023-09-03 04:18:08 +0200

    poprawione błędy
    poprawienie błędów

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'kochambardzolinuxa'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
@@ -20,12 +20,22 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
+    
+    with app.app_context():
+        db.create_all()
 
-    cerate_database(app)
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
-def cerate_database(app):
-    if not path('website/' + DB_NAME):
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
